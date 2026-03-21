9769f4a96914f7b28725624f2b3b2d93850f06b2
UMU618 <umu618@hotmail.com>
2026-03-21 11:43:02 +0800

    + res

---
+++ src/umutech/MarkdownEditor/res/resource.h
@@ -0,0 +1,47 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MarkdownEditor.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDS_REGISTER                    129
+#define IDS_REGISTER_SUCCEEDED          130
+#define IDS_UNREGISTER_SUCCEEDED        131
+#define IDR_HTML_LIGHT                  201
+#define IDR_HTML_DARK                   202
+#define IDR_HTML_GREEN                  203
+#define IDR_HTML_YELLOW                 204
+#define IDR_TABTOOLBAR                  221
+#define IDR_TAB_MENU                    222
+#define IDC_SYSLINK_FEEDBACK            1000
+#define IDC_VERSION                     1001
+#define ID_FILE_RECENTFILES_CLEAR       32772
+#define ID_EDIT_FONT                    32773
+#define ID_VIEW_EDIT                    32774
+#define ID_VIEW_PREVIEW                 32775
+#define ID_VIEW_HORIZONTAL_LAYOUT       32776
+#define ID_VIEW_MOVE_SPLITTER           32777
+#define ID_DOCUMENT_CLOSE               32778
+#define ID_DOCUMENT_CLOSE_ALL           32779
+#define ID_SHOW_VIEWS                   32780
+#define ID_THEME_LIGHT                  32781
+#define ID_THEME_DARK                   32782
+#define ID_THEME_GREEN                  32783
+#define ID_THEME_YELLOW                 32784
+#define ID_TOOL_REGISTER                32801
+#define ID_OPEN_IN_EXPLORER             32802
+#define ID_OPEN_IN_NOTEPAD              32803
+#define ID_COPY_FULL_PATH               32804
+#define ID_COPY_FILENAME                32805
+#define ID_COPY_DIRECTORY_PATH          32806
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        223
+#define _APS_NEXT_COMMAND_VALUE         32807
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
