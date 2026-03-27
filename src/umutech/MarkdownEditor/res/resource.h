6f56e33301e3433546b6cebe7f58aab85bd9aa8b
UMU618 <umu618@hotmail.com>
2026-03-28 00:16:00 +0800

    + Renderer: cmark-gfm, Hoedown, MD4C

--- src/umutech/MarkdownEditor/res/resource.h
+++ src/umutech/MarkdownEditor/res/resource.h
@@ -20,32 +20,37 @@
 #define IDC_SYSLINK_FEEDBACK            1000
 #define IDC_VERSION                     1001
 #define ID_FILE_RECENTFILES_CLEAR       32772
-#define ID_EDIT_FONT                    32773
-#define ID_VIEW_EDIT                    32774
-#define ID_VIEW_PREVIEW                 32775
+#define ID_VIEW_EDIT                    32773
+#define ID_VIEW_PREVIEW                 32774
+#define ID_VIEW_SOFTBREAK               32775
 #define ID_VIEW_HORIZONTAL_LAYOUT       32776
 #define ID_VIEW_MOVE_SPLITTER           32777
 #define ID_DOCUMENT_CLOSE               32778
 #define ID_DOCUMENT_CLOSE_ALL           32779
 #define ID_SHOW_VIEWS                   32780
-#define ID_THEME_LIGHT                  32781
-#define ID_THEME_DARK                   32782
-#define ID_THEME_GREEN                  32783
-#define ID_THEME_YELLOW                 32784
-#define ID_THEME_VSCODE                 32785
+#define ID_RENDERER_CMARK_GFM           32781
+#define ID_RENDERER_HOEDOWN             32782
+#define ID_RENDERER_MD4C                32783
+#define ID_THEME_LIGHT                  32791
+#define ID_THEME_DARK                   32792
+#define ID_THEME_GREEN                  32793
+#define ID_THEME_YELLOW                 32794
+#define ID_THEME_VSCODE                 32795
 #define ID_TOOL_REGISTER                32801
 #define ID_OPEN_IN_EXPLORER             32802
 #define ID_OPEN_IN_NOTEPAD              32803
 #define ID_COPY_FULL_PATH               32804
 #define ID_COPY_FILENAME                32805
 #define ID_COPY_DIRECTORY_PATH          32806
+#define ID_TABHEADER_CLOSE_OTHERS       32807
+#define ID_TABHEADER_CLOSE_UNCHANGED    32808
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        223
-#define _APS_NEXT_COMMAND_VALUE         32807
+#define _APS_NEXT_COMMAND_VALUE         32809
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
